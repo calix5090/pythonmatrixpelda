bf5342245b3526f1078f9dd32e9d7034a53f2d4d
calix50900 <toth.gyorgy73@gmail.com>
2023-03-13 11:27:36 +0100

    new file:   env/Lib/site-packages/_distutils_hack/__init__.py

---
+++ matrix.py
@@ -0,0 +1,27 @@
+import random
+
+def matrix_kiir(m,space=3):
+    s=len(m)
+    o=len(m[0])
+    #print ("sor =", s)
+    #print("oszlop =", o)
+
+    for i in range(s):
+        for j in range(o):
+            print("{:^5}".format(m[i][j]),end="")
+        print()
+
+
+s=3
+o=4
+
+matrix=[[0]*o for i in range(s)]
+
+e=0
+for i in range(s):
+    for j in range(o):
+        matrix[i][j]=e
+        e=e+1
+
+#print(matrix)
+matrix_kiir(matrix)
